058c55a6f7f2e038f485fb436e842b9658c38ea9
AmyFoosh <amyfoosh@gmail.com>
2023-07-09 01:30:10 -0400

    Added functionality to search product menu
    Search menu can filter products list by its category,
    showing only products that match that category.

--- src/js/view.js
+++ src/js/view.js
@@ -6,19 +6,33 @@ class View {
 
         // -- LOAD HTML ELEMENTS --
 
-        // Access to user interaction.
+        // Access to elements on Add Product.
         this.productCodeInput = document.getElementById("product-code-input");
         this.productNameInput = document.getElementById("product-name-input");
         this.productPriceInput = document.getElementById("product-price-input");
         this.productCategorySelect = document.getElementById("product-category-select");
         this.productBtn = document.getElementById("product-btn");
 
+        // Acces to elements on Search Product.
+        this.searchCategoryInput = document.getElementById("search-category-input");
+        this.searchCategorySelect = document.getElementById("search-category-select");
+        this.clearBtn = document.getElementById("clear-btn");
+
+        this.addCategoryItems(this.productCategorySelect);
+        this.addCategoryItems(this.searchCategorySelect);
+
         // Display list.
         this.productsList = document.getElementById("products-container");
 
         // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
 
-        // -- ADD LISTENERS --
+        // This is a temporary list used to filter products.
+        this.products = [];
+        this.sortModeAlphabetical = true;
+
+        // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+
+        // -- ADD LISTENERS (for add product) --
 
         // Add product when clicking button.
         this.productBtn.addEventListener("click", (e) => {
@@ -36,17 +50,141 @@ class View {
             if (e.ctrlKey && e.key === "s") e.preventDefault();
         });
 
-        // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+        // -- ADD LISTENERS (for search product) --
 
-        // This is a temporary list used to filter products.
-        this.products = [];
-        this.sortModeAlphabetical = true;
+        // Filter product by its category.
+        this.searchCategorySelect.addEventListener("change", (e) => {
+
+            // Get category value.
+            let categorySelected =
+                this.searchCategorySelect.options
+                [this.searchCategorySelect.selectedIndex].value;
+
+            console.log("Category: " + categorySelected);
+
+            // Load entire list again if value is "all".
+            if (categorySelected === "all") {
+
+                this.renderProductsList(this.products, false);
+                return;
+            }
+
+            // Filter items based on category selected if value isn't "all".
+            let arr = this.products.filter((product) => {
+
+                console.log(product.category);
+
+                // Return value in case of match.
+                if (product.category === categorySelected) return product;
+            });
+
+            // Render list with all mathces.
+            this.renderProductsList(arr, false);
+        });
+
+        // Clear fields on search product menu.
+        this.clearBtn.addEventListener("click", (e) => {
+
+            // Reset values.
+            this.searchCategoryInput.value = "";
+            this.searchCategorySelect.selectedIndex = 0;
+
+            // Reload list.
+            this.renderProductsList(this.products);
+        });
 
         // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
     }
 
+    addCategoryItems(selectHTML) {
+
+        // Create all categories for category options.
+        let categoryItems = {
+
+            all: "all",
+            beauty: "beauty",
+            kitchen: "kitchen",
+            bedroom: "bedroom",
+            bathroom: "bathroom",
+            garden: "garden",
+            lights: "lights",
+            tools: "tools",
+            food: "food",
+            shoes: "shoes",
+            clothes: "clothes",
+            pets: "pets",
+            toys: "toys",
+            plushies: "plushies",
+            furniture: "furniture",
+            homeAppliances: "home appliances",
+            camping: "camping",
+            sports: "sports"
+        };
+
+        // Create options elements.
+        let allCategory = document.createElement("option");
+        let beautyCategory = document.createElement("option");
+        let kitchenCategory = document.createElement("option");
+        let bedroomCategory = document.createElement("option");
+        let bathroomCategory = document.createElement("option");
+        let gardenCategory = document.createElement("option");
+        let lightsCategory = document.createElement("option");
+        let toolsCategory = document.createElement("option");
+        let foodCategory = document.createElement("option");
+        let shoesCategory = document.createElement("option");
+        let clothesCategory = document.createElement("option");
+        let petsCategory = document.createElement("option");
+        let toysCategory = document.createElement("option");
+        let plushiesCategory = document.createElement("option");
+        let furnitureCategory = document.createElement("option");
+        let homeAppliancesCategory = document.createElement("option");
+        let campingCategory = document.createElement("option");
+        let sportsCategory = document.createElement("option");
+
+        // Add text.
+        allCategory.innerText = categoryItems.all;
+        beautyCategory.innerText = categoryItems.beauty;
+        kitchenCategory.innerText = categoryItems.kitchen;
+        bedroomCategory.innerText = categoryItems.bedroom;
+        bathroomCategory.innerText = categoryItems.bathroom;
+        gardenCategory.innerText = categoryItems.garden;
+        lightsCategory.innerText = categoryItems.lights;
+        toolsCategory.innerText = categoryItems.tools;
+        foodCategory.innerText = categoryItems.food;
+        shoesCategory.innerText = categoryItems.shoes;
+        clothesCategory.innerText = categoryItems.clothes;
+        petsCategory.innerText = categoryItems.pets;
+        toysCategory.innerText = categoryItems.toys;
+        plushiesCategory.innerText = categoryItems.plushies;
+        furnitureCategory.innerText = categoryItems.furniture;
+        homeAppliancesCategory.innerText = categoryItems.homeAppliances;
+        campingCategory.innerText = categoryItems.camping;
+        sportsCategory.innerText = categoryItems.sports;
+
+        // Add options elements to category.
+        selectHTML.appendChild(allCategory);
+        selectHTML.appendChild(beautyCategory);
+        selectHTML.appendChild(kitchenCategory);
+        selectHTML.appendChild(bedroomCategory);
+        selectHTML.appendChild(bathroomCategory);
+        selectHTML.appendChild(gardenCategory);
+        selectHTML.appendChild(lightsCategory);
+        selectHTML.appendChild(toolsCategory);
+        selectHTML.appendChild(foodCategory);
+        selectHTML.appendChild(shoesCategory);
+        selectHTML.appendChild(clothesCategory);
+        selectHTML.appendChild(petsCategory);
+        selectHTML.appendChild(toysCategory);
+        selectHTML.appendChild(plushiesCategory);
+        selectHTML.appendChild(furnitureCategory);
+        selectHTML.appendChild(homeAppliancesCategory);
+        selectHTML.appendChild(campingCategory);
+        selectHTML.appendChild(sportsCategory);
+    }
+
     addProduct() {
 
+        // Call function to create product.
         app.createProduct(
             this.productCodeInput.value,
             this.productNameInput.value,
@@ -54,10 +192,15 @@ class View {
             this.productCategorySelect.value
         );
 
+        // Clear values for Add Product menu.
         this.productCodeInput.value = "";
         this.productNameInput.value = "";
         this.productPriceInput.value = "";
         this.productCategorySelect.selectedIndex = 0;
+
+        // Clear values for Search Product menu.
+        this.searchCategoryInput.value = "";
+        this.searchCategorySelect.selectedIndex = 0;
     }
 
     // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
@@ -65,15 +208,17 @@ class View {
     // -- OPERATIONS --
 
     // Load all products stored.
-    renderProductsList(products) {
+    renderProductsList(products, updateList = true) {
+
+        console.log("updateList: " + updateList);
 
         // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
 
         // -- CREATING TABLE HEAD ELEMENTS --
 
         // Add changes to temporary list.
-        this.products = products;
-        console.log("Temporary: " + this.products);
+        if (updateList) this.products = products;
+        console.log(this.products);
 
         // Clear entire list.
         this.productsList.innerHTML = "";
